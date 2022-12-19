c1a82305d56abdb3094974cf6d00e77ce5118154
Martin Lenart <martin_lenart@icloud.com>
2022-12-19 11:07:04 +0100

    Add project files.

---
+++ Assignment_A1_02/Services/OpenWeatherService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json; 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+using Assignment_A1_02.Models;
+
+namespace Assignment_A1_02.Services
+{
+    public class OpenWeatherService
+    {
+        HttpClient httpClient = new HttpClient();
+
+        // Your API Key
+        readonly string apiKey = ""; 
+
+        //Event declaration
+        public event EventHandler<string> WeatherForecastAvailable;
+        protected virtual void OnWeatherForecastAvailable (string message)
+        {
+            WeatherForecastAvailable?.Invoke(this, message);
+        }
+        public async Task<Forecast> GetForecastAsync(string City)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+            
+            //Event code here to fire the event
+            //Your code
+            return forecast;
+        }
+        public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+
+            //Event code here to fire the event
+            //Your code
+            return forecast;
+        }
+        private async Task<Forecast> ReadWebApiAsync(string uri)
+        {
+            //Read the response from the WebApi
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            WeatherApiData wd = await response.Content.ReadFromJsonAsync<WeatherApiData>();
+
+            //Convert WeatherApiData to Forecast using Linq.
+            //Your code
+            var forecast = new Forecast(); //dummy to compile, replaced by your own code
+            return forecast;
+        }
+        private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+}
